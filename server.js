9e08e7c2bdd8c26351bc881a65ef465115ba4235
willvdb <willvdb9141@gmail.com>
2015-03-18 08:59:58 -0600

    Fixing the getcity path

--- server.js
+++ server.js
@@ -17,67 +17,68 @@ app.use('/', express.static('./public', {
   maxAge: 60 * 60 * 1000
 }));
 
-app.get('/getcity', function(req, res){
-  fs.readFile("cities.dat.txt", function(err, data) {
-    if (err) {
-      res.writeHead(200);
-      res.end("There was an error getting your cities...")
-    } else {
-      var resultArray = [];
-      if (!isEmpty(urlObj.query["q"])) {
-        var cities = data.toString().split('\n');
-        var myRe = new RegExp("^" + urlObj.query["q"]);
-        for (i = 0; i < cities.length; i++) {
-          var result = cities[i].search(myRe);
-          if (result != -1) {
-            var obj = {
-              "city": cities[i]
-            }
-            resultArray.push(obj);
-          }
-        }
-      }
-      res.writeHead(200);
-      res.end(JSON.stringify(resultArray));
-    }
-  });
-});
-
-app.post('/comment', function(req, res){
-  var jsonData = "";
-  req.on('data', function(chunk) {
-    jsonData += chunk;
-  });
-  req.on('end', function() {
-    var reqObj = JSON.parse(jsonData);
-    var mongo_client = mongo.MongoClient;
-    mongo_client.connect("mongodb://localhost/weather", function(err, db) {
-      if (err) {
-        console.log("error");
-      } else {
-        db.collection('comments').insert(reqObj, function(err, records) {
-          res.writeHead(200);
-          res.end("");
-        });
-      }
-    });
-  });
-});
-
-app.get('/comment', function(req, res){
-  var mongo_client = mongo.MongoClient;
-  mongo_client.connect("mongodb://localhost/weather", function(err, db){
-    if (err) {
-      console.log("error");
-    } else {
-      db.collection("comments", function(err, comments){
-        comments.find(function(err, items){
-          items.toArray(function(err, item_array){
+app.get('/getcity', function(req, res) {
+      var urlObj = url.parse(req.url, true, false);
+        fs.readFile("cities.dat.txt", function(err, data) {
+          if (err) {
             res.writeHead(200);
-            res.end(JSON.stringify(item_array));
-          });
+            res.end("There was an error getting your cities...")
+          } else {
+            var resultArray = [];
+            if (!isEmpty(urlObj.query["q"])) {
+              var cities = data.toString().split('\n');
+              var myRe = new RegExp("^" + urlObj.query["q"]);
+              for (i = 0; i < cities.length; i++) {
+                var result = cities[i].search(myRe);
+                if (result != -1) {
+                  var obj = {
+                    "city": cities[i]
+                  }
+                  resultArray.push(obj);
+                }
+              }
+            }
+            res.writeHead(200);
+            res.end(JSON.stringify(resultArray));
+          }
         });
       });
-    }
-  });
-});
+
+    app.post('/comment', function(req, res) {
+      var jsonData = "";
+      req.on('data', function(chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function() {
+        var reqObj = JSON.parse(jsonData);
+        var mongo_client = mongo.MongoClient;
+        mongo_client.connect("mongodb://localhost/weather", function(err, db) {
+          if (err) {
+            console.log("error");
+          } else {
+            db.collection('comments').insert(reqObj, function(err, records) {
+              res.writeHead(200);
+              res.end("");
+            });
+          }
+        });
+      });
+    });
+
+    app.get('/comment', function(req, res) {
+      var mongo_client = mongo.MongoClient;
+      mongo_client.connect("mongodb://localhost/weather", function(err, db) {
+        if (err) {
+          console.log("error");
+        } else {
+          db.collection("comments", function(err, comments) {
+            comments.find(function(err, items) {
+              items.toArray(function(err, item_array) {
+                res.writeHead(200);
+                res.end(JSON.stringify(item_array));
+              });
+            });
+          });
+        }
+      });
+    });
